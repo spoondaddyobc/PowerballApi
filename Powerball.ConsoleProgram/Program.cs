544ef4ed2fed38236547b54e6e7416308e696dd1
Tyree Barron <tyree.barron@gmail.com>
2017-06-18 15:28:51 -0400

    Helper folder and Parser class created.

---
+++ Powerball.ConsoleProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowerballApi;
+using PowerballApi.Controllers;
+using PowerballApi.Models;
+
+namespace Powerball.ConsoleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCall = new PowerballController();
+
+            var numbers = new List<PowerballSet>();
+            var download = testCall.GetPowerballNumbers();
+            foreach (var item in download)
+            {
+                numbers.Add(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
