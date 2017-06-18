f1af66e5134175b1d09335903ecd0cf02211c644
Justin Aurand <justin.aurand@swiftcapital.com>
2017-06-18 18:51:21 -0400

    Tested null/whitespace cases for powerball parser

---
+++ PowerballApi.Tests/PowerballParser/PowerballParserTests.cs
@@ -0,0 +1,31 @@
+﻿namespace PowerballApi.Tests.PowerballParser
+{
+	using System.Collections.Generic;
+	using Helper;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Models;
+
+	[TestClass]
+	public class PowerballParserTests
+	{
+		[TestMethod]
+		public void WhenFileIsNull_ReturnEmptyList()
+		{
+			var sot = new PowerballParser();
+
+			var result = sot.Parse(null);
+
+			CollectionAssert.AreEqual(result, new List<PowerballSet>());
+		}
+
+		[TestMethod]
+		public void WhenFileIsEmpty_ReturnEmptyList()
+		{
+			var sot = new PowerballParser();
+
+			var result = sot.Parse(string.Empty);
+
+			CollectionAssert.AreEqual(result, new List<PowerballSet>());
+		}
+	}
+}
