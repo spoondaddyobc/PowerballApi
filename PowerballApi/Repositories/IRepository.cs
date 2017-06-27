6336a6c6dce76e709b57e3bbd180e62fc7fbe78c
Tyree Barron <tyree.barron@gmail.com>
2017-06-27 08:07:47 -0400

    Repository updates
    Still in progress.

--- PowerballApi/Repositories/IRepository.cs
+++ PowerballApi/Repositories/IRepository.cs
@@ -1,14 +1,14 @@
 ﻿namespace PowerballApi.Api.Repositories
 {
-	using System.Collections.Generic;
-	using Models;
+    using System.Collections.Generic;
+    using Models;
 
-	public interface IRepository <T> where T : class 
-	{
-		List<T> Get();
+    public interface IRepository<T> where T : class
+    {
+        List<T> Get();
 
-	    T GetById();
+        T GetById(string id);
 
-	    List<T> GetByRange();
-	}
+        List<T> GetByRange(string idStart, string idEnd);
+    }
 }
--- PowerballApi/Repositories/PowerballRepository.cs
+++ PowerballApi/Repositories/PowerballRepository.cs
@@ -1,62 +1,87 @@
 ﻿using System;
+using System.Linq;
 
 namespace PowerballApi.Api.Repositories
 {
-	using System.Collections.Generic;
-	using Helpers.Cacher;
-	using Helpers.HttpHandler;
-	using Helpers.PowerballParser;
-	using Models;
+    using System.Collections.Generic;
+    using Helpers.Cacher;
+    using Helpers.HttpHandler;
+    using Helpers.PowerballParser;
+    using Models;
 
-	public class PowerballRepository : IRepository<PowerballSet>
-	{
-		public string CacheName { get; set; } = @"PowerballData";
-		public int DaysUntilStale { get; set; } = 1;
-		public string PowerballUrl { get; set; } = @"http://www.powerball.com/powerball/winnums-text.txt";
+    public class PowerballRepository : IRepository<PowerballSet>
+    {
+        public string CacheName { get; set; } = @"PowerballData";
+        public int DaysUntilStale { get; set; } = 1;
+        public string PowerballUrl { get; set; } = @"http://www.powerball.com/powerball/winnums-text.txt";
 
-		private readonly ICacher _cacher;
-		private readonly IHttpHandler _httpHandler;
-		private readonly IPowerballParser _parser;
+        private readonly ICacher _cacher;
+        private readonly IHttpHandler _httpHandler;
+        private readonly IPowerballParser _parser;
 
-		public PowerballRepository()
-		{
-			_cacher = new Cacher();
-			_httpHandler = new HttpHandler();
-			_parser = new PowerballParser();
-		}
+        public PowerballRepository()
+        {
+            _cacher = new Cacher();
+            _httpHandler = new HttpHandler();
+            _parser = new PowerballParser();
+        }
 
-		public PowerballRepository(ICacher cacher, IHttpHandler httpHandler, IPowerballParser parser)
-		{
-			_cacher = cacher;
-			_httpHandler = httpHandler;
-			_parser = parser;
-		}
+        public PowerballRepository(ICacher cacher, IHttpHandler httpHandler, IPowerballParser parser)
+        {
+            _cacher = cacher;
+            _httpHandler = httpHandler;
+            _parser = parser;
+        }
 
-		public List<PowerballSet> Get()
-		{
-			var cache = _cacher.Get(CacheName);
-			if (cache != null)
-				return (List<PowerballSet>)cache;
+        public List<PowerballSet> Get()
+        {
+            var cache = _cacher.Get(CacheName);
+            if (cache != null)
+                return (List<PowerballSet>)cache;
 
-			return GetFreshDrawings();
-		}
+            return GetFreshDrawings();
+        }
 
-	    public PowerballSet GetById()
-	    {
-	        throw new NotImplementedException();
-	    }
+        public PowerballSet GetById(string id)
+        {
+            var cache = _cacher.Get(CacheName);
+            List<PowerballSet> dataSet;
+            if (cache != null)
+            {
+                dataSet = (List<PowerballSet>)cache;
+            }
+            else
+            {
+                dataSet = GetFreshDrawings();
+            }
+            var result = dataSet.FirstOrDefault(p => p.Date == id);
+            return result;
+        }
 
-	    public List<PowerballSet> GetByRange()
-	    {
-	        throw new NotImplementedException();
-	    }
+        public List<PowerballSet> GetByRange(string idBegin, string idEnd)
+        {
+            var cache = _cacher.Get(CacheName);
+            List<PowerballSet> dataSet;
+            if (cache != null)
+            {
+                dataSet = (List<PowerballSet>)cache;
+            }
+            else
+            {
+                dataSet = GetFreshDrawings();
+            }
 
-	    private List<PowerballSet> GetFreshDrawings()
-		{
-			var file = _httpHandler.GetStringAsync(PowerballUrl).Result;
-			var results = _parser.Parse(file);
-			_cacher.Set(CacheName, results, DaysUntilStale);
-			return results;
-		}
-	}
+            var upperLimit = dataSet.FindAll(p => DateTime.Parse(p.Date) >= DateTime.Parse(idBegin));
+            var results = upperLimit.FindAll(p => DateTime.Parse(p.Date) <= DateTime.Parse(idEnd));
+            return results.ToList();
+        }
+
+        private List<PowerballSet> GetFreshDrawings()
+        {
+            var file = _httpHandler.GetStringAsync(PowerballUrl).Result;
+            var results = _parser.Parse(file);
+            _cacher.Set(CacheName, results, DaysUntilStale);
+            return results;
+        }
+    }
 }
